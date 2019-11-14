3dd27b974cf28ac27719124d13ac4cc571faff24
victor_silvestre <victor.silvestre0@gmail.com>
2019-11-14 14:22:27 +0100

    Nickel ! local storage effectué (indice en TP... faire une fonction load() et save()sur le service passant par la fonction localStorage

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -6,8 +6,10 @@ import {TodoItemData} from '../dataTypes/TodoItemData';
 @Injectable()
 export class TodoService {
 
-  private todoListSubject = new BehaviorSubject<TodoListData>( {label: 'TodoList', items: []} );
+  private todoListSubject = new BehaviorSubject<TodoListData>( {label: 'TodoList', items: [] = this.load()});
 
+  undo: [[]] = [[]];
+  redo: [[]] = [[]];
   constructor() { }
 
   getTodoListDataObserver(): Observable<TodoListData> {
@@ -20,6 +22,7 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label, isDone: I.isDone}) )
     });
+    this.save();
   }
 
 
@@ -29,6 +32,7 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label: I.label, isDone}) )
     });
+    this.save();
   }
 
   appendItems( ...items: TodoItemData[] ) {
@@ -37,6 +41,7 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: [...tdl.items, ...items]
     });
+    this.save();
   }
 
   removeItems( ...items: TodoItemData[] ) {
@@ -45,6 +50,7 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: tdl.items.filter( I => items.indexOf(I) === -1 )
     });
+    this.save();
   }
 
   removeAllItemChecked() {
@@ -53,6 +59,20 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: tdl.items.filter( I => I.isDone === false )
     });
+  this.save();
+
+
   }
+
+  save() {
+    const tdl = this.todoListSubject.getValue();
+    console.log(tdl.label);
+    localStorage.setItem(tdl.label, JSON.stringify(tdl.items));
+  }
+
+  load() {
+    return JSON.parse(localStorage.getItem('TodoList'));
+  }
+
 
 }
